49af0cbe9b3ed44007372d185f8d7adf8573e484
aryawardhana15 <wardhanahafiz567@gmail.com>
2025-01-31 22:38:42 +0700

    first commit

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import LogoImg from "../assets/images/logo-smilekids.png"
+import { LinkData } from "../assets/data/dummydata"
+import { NavLink } from "react-router-dom"
+import { BiShoppingBag } from "react-icons/bi"
+import { HiOutlineMenuAlt1, HiViewGrid } from "react-icons/hi"
+
+export const Header = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <header className='bg-yellow-50 py-4 text-black sticky z-50 shadow-md top-0 left-0 w-full ' >
+        <div className='container flex justify-between items-center '>
+          <div className='logo flex items-center gap-0 '>
+            <img src={LogoImg} alt='logo' className='h-9 w-14' /> 
+            <h1 className=' gap-0.5 text-2xl font-semibold text-yellow-900'>SMILE KIDS</h1>
+            {/* <div className='category flex items-center text-sm gap-6'>
+              <HiViewGrid size={20} />
+              <span>category</span>
+            </div> */}
+          </div>
+          <nav className={open ? "mobile-view" : "desktop-view"}>
+            <ul className='flex items-center gap-6'>
+              {LinkData.map((link) => (
+                <li key={link.id} onClick={() => setOpen(null)}>
+                  <NavLink className={({ isActive }) => (isActive ? "text-primary text-sm" : "text-[15px]")} to={link.url}>
+                    {link.title}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className='account flex items-center gap-5'>
+            {/* <button>
+              <BiShoppingBag size={25} />
+            </button> */}
+            <button>Login</button>{" "}
+            <button className='open-menu' onClick={() => setOpen(!open)}>
+              <HiOutlineMenuAlt1 size={25} />
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,94 @@
+import React from "react"
+import heroImg from "../components/assets/images/hero.png"
+import heroImgback from "../components/assets/images/hero-shape-purple.png"
+import { FiSearch } from "react-icons/fi"
+import { BsFillLightningChargeFill } from "react-icons/bs"
+import { FaGraduationCap } from "react-icons/fa"
+import { About } from "./About"
+import { Courses } from "./Courses"
+import { Instructor } from "./Instructor"
+import { Blog } from "./Blog"
+
+export const Home = () => {
+  return (
+    <>
+      <HomeContent />
+      <About />
+      <br />
+      <br />
+      <br />
+      <Courses />
+      <Instructor />
+      <Blog />
+    </>
+  )
+}
+
+export const HomeContent = () => {
+  return (
+    <>
+      <section className='bg-yellow-300 py-10 h-[92vh] md:h-full'>
+        <div className='container'>
+          <div className='flex items-center justify-center md:flex-col'>
+            <div className='left w-1/2 text-black md:w-full'>
+              <h1 className='text-4xl leading-tight text-black font-semibold'>
+                Welcome to Smile Kids <br /> Fun and Interactive Learning for Kids
+              </h1>
+              <h3 className='text-lg mt-3'>
+                Unlock endless educational opportunities with 60+ expert instructors
+              </h3>
+              <span className='text-[14px]'>Get two passes for only $240 with unlimited class access!</span>
+
+              {/* <div className='relative text-gray-600 focus-within:text-gray-400 mt-5'>
+                <input 
+                  type='search' 
+                  className='py-3 text-sm bg-white rounded-md pl-10 focus:outline-none' 
+                  placeholder='Search for classes...' 
+                  autocomplete='off' 
+                />
+                <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
+                  <button type='submit' className='p-1 focus:outline-none focus:shadow-outline'>
+                    <FiSearch />
+                  </button>
+                </span>
+              </div> */}
+              <span className='text-[14px]'>
+                Explore various fun learning subjects and choose the right class for your child!
+              </span>
+            </div>
+
+            <div className='right w-1/2 md:w-full relative'>
+              <div className='images relative'>
+                <img src={heroImgback} alt='' className='absolute top-32 left-10 w-96 md:left-10' />
+                <div className='img h-[85vh] w-full'>
+                  <img src={heroImg} alt='' className='h-full w-full object-contain z-20 relative' />
+                </div>
+              </div>
+              <div className='content'>
+                <button className='bg-white shadow-md absolute top-56 left-0 z-30 p-2 flex items-center rounded-md'>
+                  <div className='icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-orange-400'>
+                    <BsFillLightningChargeFill size={25} />
+                  </div>
+                  <div className='text flex flex-col items-start px-4'>
+                    <span className='text-sm text-black'>Congratulations!</span>
+                    <span className='text-[12px]'>Your child’s enrollment is complete.</span>
+                  </div>
+                </button>
+
+                <button className='bg-white shadow-md absolute top-56 -right-32 z-30 p-2 md:top-96 md:-right-5 flex items-center rounded-md'>
+                  <div className='icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-orange-400'>
+                    <FaGraduationCap size={25} />
+                  </div>
+                  <div className='text flex flex-col items-start px-4'>
+                    <span className='text-sm text-black'>450K+</span>
+                    <span className='text-[12px]'>Students Assisted</span>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
